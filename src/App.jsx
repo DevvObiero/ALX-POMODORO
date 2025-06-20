bcbb5972d2e230eb1810e3ea6707280a495e87b2
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-06-20 13:53:43 +0300

    added the radient clors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+   
+    <div className="text-[#c9ccea] flex flex-col items-center justify-center min-h-screen bg-[radial-gradient(circle,_#012754_0%,_#06F076_30%,_#012754_100%)]">
+         <h1>POMODORO</h1>
+      <p>Maximize your productivity</p>
+</div>
+
+ 
   )
 }
 
